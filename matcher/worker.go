a35b0791c61f43932201df82859f3ba90783c42f
Lizhi <lizhi2754@idss-cn.com>
2024-03-08 10:40:26 +0800

    recode worker.go

--- matcher/worker.go
+++ matcher/worker.go
@@ -50,70 +50,14 @@ func (p *Worker) Init() error {
 func (p *Worker) Run() {
 	log.Infof("worker running with name [%s], value regexs %d, column dicts %d",
 		p.Name, len(p.ValueRegs), len(p.ColDicts))
-	p.CountMsg = 0
-	p.CountMatchRegex = 0
-	p.CountMatchDict = 0
+	p.CountMsg, p.CountMatchRegex, p.CountMatchDict = 0, 0, 0
 
-	var reg_matched, dict_magched = false, false
-	var reg_lable, dict_lable = "", ""
 	for m := range p.Msgch {
 		p.CountMsg++
-		reg_matched, dict_magched = false, false
 		// log.Debugf(m.Size(), len(m.Data))
 
-		// 依次匹配正则表达式
-		reg_lable = ""
-		for i, r := range p.rs {
-			loc := r.FindIndex(m.Data)
-			if loc != nil {
-				// log.Debugf("regex find rule %d with position %v", i, loc)
-				if len(reg_lable) > 0 {
-					reg_lable += ", "
-				}
-				b, _ := json.Marshal(loc)
-				reg_lable += fmt.Sprintf(`{"lable": "%s", "position": %s}`, p.ValueRegs[i].InfoType, b)
-				atomic.AddUint64(&p.ValueRegs[i].CountMatch, 1)
-
-				p.CountMatchRegex++
-				reg_matched = true
-			}
-		}
-
-		// 一次多模式匹配Dictionary
-		dict_lable = ""
-		matches := p.trie.Match(m.Data)
-		if len(matches) > 0 {
-			for i, match := range matches {
-				if i > 0 {
-					dict_lable += ", "
-				}
-				dict_lable += fmt.Sprintf(`{"lable": "%s", "match": "%s", "position": %v}`,
-					p.ColDicts[match.Pattern()].InfoType, match.Match(), match.Pos())
-				atomic.AddUint64(&p.ColDicts[match.Pattern()].CountMatch, 1)
-			}
-
-			p.CountMatchDict += uint64(len(matches))
-			dict_magched = true
-		}
-
-		// 如果匹配到了正则或字典，写入输出队列
-		if reg_matched || dict_magched {
-			if len(reg_lable) > 0 {
-				reg_lable = `, "value_regex": [` + reg_lable + `]`
-				if m.Data[len(m.Data)-1] == '}' { // 删除最后一个大括号
-					m.Data = append(m.Data[:len(m.Data)-1], []byte(reg_lable)...)
-				} else {
-					m.Data = append(m.Data, []byte(reg_lable)...)
-				}
-			}
-			if len(dict_lable) > 0 {
-				dict_lable = `, "column_dict": [` + dict_lable + `] }`
-				if m.Data[len(m.Data)-1] == '}' { // 删除最后一个大括号
-					m.Data = append(m.Data[:len(m.Data)-1], []byte(dict_lable)...)
-				} else {
-					m.Data = append(m.Data, []byte(dict_lable)...)
-				}
-			}
+		// 处理消息
+		if matched := p.proccessMsg(m); matched {
 			p.Outch <- m
 		}
 
@@ -127,4 +71,77 @@ func (p *Worker) Run() {
 func (p *Worker) Dump() []byte {
 	b, _ := json.Marshal(p)
 	return b
+}
+
+func (p *Worker) proccessMsg(m *nats.Msg) bool {
+	// 依次匹配正则表达式
+	reg_lable, reg_matched := p.matchReg(m)
+
+	// 一次多模式匹配Dictionary
+	dict_lable, dict_magched := p.matchDict(m)
+
+	// 如果匹配到了正则或字典，写入输出队列
+	if reg_matched || dict_magched {
+		if len(reg_lable) > 0 {
+			reg_lable = `, "value_regex": [` + reg_lable + `]`
+			if m.Data[len(m.Data)-1] == '}' { // 删除最后一个大括号
+				m.Data = append(m.Data[:len(m.Data)-1], []byte(reg_lable)...)
+			} else {
+				m.Data = append(m.Data, []byte(reg_lable)...)
+			}
+		}
+		if len(dict_lable) > 0 {
+			dict_lable = `, "column_dict": [` + dict_lable + `] }`
+			if m.Data[len(m.Data)-1] == '}' { // 删除最后一个大括号
+				m.Data = append(m.Data[:len(m.Data)-1], []byte(dict_lable)...)
+			} else {
+				m.Data = append(m.Data, []byte(dict_lable)...)
+			}
+		}
+		return true
+	}
+
+	return false
+}
+
+func (p *Worker) matchReg(m *nats.Msg) (string, bool) {
+	// 依次匹配正则表达式
+	reg_lable, reg_matched := "", false
+	for i, r := range p.rs {
+		loc := r.FindIndex(m.Data)
+		if loc != nil {
+			// log.Debugf("regex find rule %d with position %v", i, loc)
+			if len(reg_lable) > 0 {
+				reg_lable += ", "
+			}
+			b, _ := json.Marshal(loc)
+			reg_lable += fmt.Sprintf(`{"lable": "%s", "position": %s}`, p.ValueRegs[i].InfoType, b)
+			atomic.AddUint64(&p.ValueRegs[i].CountMatch, 1)
+
+			p.CountMatchRegex++
+			reg_matched = true
+		}
+	}
+
+	return reg_lable, reg_matched
+}
+
+func (p *Worker) matchDict(m *nats.Msg) (string, bool) {
+	dict_lable, dict_magched := "", false
+	matches := p.trie.Match(m.Data)
+	if len(matches) > 0 {
+		for i, match := range matches {
+			if i > 0 {
+				dict_lable += ", "
+			}
+			dict_lable += fmt.Sprintf(`{"lable": "%s", "match": "%s", "position": %v}`,
+				p.ColDicts[match.Pattern()].InfoType, match.Match(), match.Pos())
+			atomic.AddUint64(&p.ColDicts[match.Pattern()].CountMatch, 1)
+		}
+
+		p.CountMatchDict += uint64(len(matches))
+		dict_magched = true
+	}
+
+	return dict_lable, dict_magched
 }
